a5440f94d37871e3160998741f1e648036200621
crlstve <ecarles10@gmail.com>
2026-07-10 09:19:48 +0200

    ref: fondo animado

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -3,7 +3,7 @@
         <span class="dark:text-white text-xs font-bold"><?= 'c.esteve  ' . date("Y"); ?></span>
     </div>
 </footer>
-    <script defer type="module" src="./assets/js/waves.js"></script>
+    <script defer type="module" src="./assets/js/light-speed.js"></script>
     <script defer type="module" src="./assets/js/splide.min.js"></script>
     <script defer type="module" src="./assets/js/splide-extension-auto-scroll.min.js"></script>
 </body>
