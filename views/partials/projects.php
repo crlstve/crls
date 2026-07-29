66ccf9510fdb09955269846539625f731241e79e
crlstve <ecarles10@gmail.com>
2026-07-29 16:24:12 +0200

    maquetacion card proyecto

--- views/partials/projects.php
+++ views/partials/projects.php
@@ -1,18 +1,32 @@
     <div id="proyectos" class="col-span-12 md:col-span-6 lg:col-span-12 xl:col-span-6 flex flex-col mb-12 md:mb-0">
             <h2 data-text="experiencia" class="w-fit mx-auto font-title p-6 text-3xl md:text-2xl text-center font-bold leading-4 mb-6">proyectos</h2>
             <ul class="flex flex-col justify-center gap-4 md:gap-8 snap-y snap-mandatory px-2 xl:px-0">
-                <li class="card after:top-0 before:top-0 snap-always md:mt-6 fadeInUp visible mx-auto">
-                    <header>
-                        <h3 class="text-2xl font-title font-bold leading-4 mb-1">adopta un comercio</h3>
-                        <time class="text-sm" datetime="08/11/24">08-11-24</time>
-                        <a rel="nofollow noopener noreferrer" target="_blank" aria-label="visita adopta un comercio" href="https://adoptauncomercio.com/" class="link-right">
-                            <?= Functions::svg(); ?>
+                <li class="card relative p-2 xl:p-0 snap-always xl:col-span-6 fadeInUp visible bg-transparent backdrop-blur-sm flex flex-row justify-between">                        
+                    <a rel="nofollow noopener noreferrer" target="_blank" aria-label="visita adopta un comercio" href="https://adoptauncomercio.com/" class="link-right">
+                        <?= Functions::svg(); ?>
+                    </a>
+
+
+                    <article class="card-body">
+                        <a class="card-head" href="https://adoptauncomercio.com/" rel="nofollow noopener noreferrer" target="_blank" aria-label="visita adopta un comercio">
+                            <span class="card-title-line"></span>       
+                            <h3 class="card-title dark:text-dark">adopta un comercio</h3>    
                         </a>
-                    </header>
-                    <article>
-                        <p class="text-base">
-                           Plataforma para ayudar a los comercios afectados por la <b>DANA de València de 2024</b>. El desarrollo fue hecho con <b>Wordpress</b>, para el cual cree un <b>tema propio</b> que automatizaba el envío de correos para avisar a los usuarios, así como un sistema de altas y vinculación mediante formularios de contacto.
-                        </p>
+                        <div class="card-content">
+                            <p class="text-sm text-justify ">
+                            Plataforma para ayudar a los comercios afectados por la <b>DANA de València de 2024</b>. El desarrollo fue hecho con <b>Wordpress</b>, para el cual cree un <b>tema propio</b> que automatizaba el envío de correos para avisar a los usuarios, así como un sistema de altas y vinculación mediante formularios de contacto.
+                            </p>
+                            <ul class="tags" aria-label="Tecnologías">
+                                    <li class="tag">WordPress</li>
+                                    <li class="tag">API REST</li>
+                                    <li class="tag">UX</li>
+                                    <li class="tag">UI</li>
+                                    <li class="tag">PHP</li>
+                                    <li class="tag">MySQL</li>
+
+                                </ul>
+                            <time class="text-sm" datetime="08/11/24">08-11-24</time>
+                        </div>
                     </article>
                 </li>
             </ul>
